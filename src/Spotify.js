ab7a07f3826db0b1632914de98bf55e04eb80954
AnantDuhan <duhananant@gmail.com>
2021-06-09 21:38:08 +0530

    ADD: Fetch accessURL and Token

--- src/Spotify.js
+++ src/Spotify.js
@@ -12,4 +12,16 @@ const scopes = [
     "user-modify-playback-state",
 ];
 
+export const getTokenFromResponse = () => {
+    return window.location.hash
+        .substring(1)
+        .split('&')
+        .reduce((initial, item) => {
+            var parts = item.split("=");
+            initial[parts[0]] = decodeURIComponent(parts[1]);
+
+            return initial;
+    }, {});
+};
+
 export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
