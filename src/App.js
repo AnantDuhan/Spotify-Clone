12d2959d3a32eef95a774c977bc225215236132d
AnantDuhan <duhananant@gmail.com>
2021-06-10 12:01:53 +0530

    ADD: Final Auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
+import { getTokenFromResponse } from './Spotify';
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from './Player';
+import { useDataLayerValue} from './DataLayer';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+
+  const [{user, token}, dispatch] = useDataLayerValue();
+
+  // run code based on a given condition
+  useEffect(() => {
+    const hash = getTokenFromResponse();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if(_token) {
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      })
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user => {
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+        });
+      });
+    }
+
+  }, []);
+
   return (
     <div className="app">
-      <Login />
+      {
+        token ? (
+          <Player spotify={spotify} />
+        ) : (
+          <Login />
+        )
+      }
     </div>
   );
 }
